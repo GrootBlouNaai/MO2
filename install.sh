f40515c6760f722fd90f59b15c2a92149d35a976
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-09 21:32:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,86 +1,80 @@
 #!/usr/bin/env bash
 
-cache_enabled="${CACHE:-1}"
-
 set -eu
 set -o pipefail
 
-script_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+SCRIPT_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+UTILS="$SCRIPT_ROOT/utils"
+DIALOG="$UTILS/dialog.sh"
+GAMESINFO="$SCRIPT_ROOT/gamesinfo"
+HANDLERS="$SCRIPT_ROOT/handlers"
+LAUNCHERS="$SCRIPT_ROOT/launchers"
+REDIRECTOR="$SCRIPT_ROOT/steam-redirector"
+STEP="$SCRIPT_ROOT/step"
+WORKAROUNDS="$SCRIPT_ROOT/workarounds"
+DOWNLOADS_CACHE="/tmp/mo2-linux-installer-downloads-cache"
+SHARED="$HOME/.local/share/modorganizer2"
 
-utils="$script_root/utils"
-dialog="$utils/dialog.sh"
-gamesinfo="$script_root/gamesinfo"
-handlers="$script_root/handlers"
-launchers="$script_root/launchers"
-redirector="$script_root/steam-redirector"
-step="$script_root/step"
-workarounds="$script_root/workarounds"
-downloads_cache=/tmp/mo2-linux-installer-downloads-cache
-shared="$HOME/.local/share/modorganizer2"
+CACHE_ENABLED="${CACHE:-1}"
+STARTED_DOWNLOAD_STEP=0
+EXPECT_EXIT=0
 
-started_download_step=0
-expect_exit=0
+mkdir -p "$DOWNLOADS_CACHE"
+mkdir -p "$SHARED"
 
-mkdir -p "$downloads_cache"
-mkdir -p "$shared"
-
-function handle_error() {
-	if [ "$expect_exit" != "1" ]; then
-		if [ "$started_download_step" == "1" ]; then
-			purge_downloads_cache
-		fi
-
-		"$dialog" \
-			errorbox \
-			"Operation canceled. Check the terminal for details"
-	fi
+handle_error() {
+    if [ "$EXPECT_EXIT" != "1" ]; then
+        if [ "$STARTED_DOWNLOAD_STEP" == "1" ]; then
+            purge_downloads_cache
+        fi
+        "$DIALOG" errorbox "Operation canceled. Check the terminal for details"
+    fi
 }
 
-function log_info() {
-	echo "INFO:" "$@" >&2
+log_info() {
+    echo "INFO:" "$@" >&2
 }
 
-function log_warn() {
-	echo "WARN:" "$@" >&2
+log_warn() {
+    echo "WARN:" "$@" >&2
 }
 
-function log_error() {
-	echo "ERROR:" "$@" >&2
+log_error() {
+    echo "ERROR:" "$@" >&2
 }
 
 trap handle_error EXIT
 
 if [ "$UID" == "0" ]; then
-	log_error "Attempted to run as root"
-	log_error "Please follow the install instructions provided at https://github.com/rockerbacon/modorganizer2-linux-installer"
-	exit 1
+    log_error "Attempted to run as root"
+    log_error "Please follow the install instructions provided at https://github.com/rockerbacon/modorganizer2-linux-installer"
+    exit 1
 fi
 
-expect_exit=1
+EXPECT_EXIT=1
 
-source "$step/check_dependencies.sh"
+source "$STEP/check_dependencies.sh"
 
-selected_game=$(source "$step/select_game.sh")
-log_info "selected game '$selected_game'"
+SELECTED_GAME=$(source "$STEP/select_game.sh")
+log_info "selected game '$SELECTED_GAME'"
 
-source "$step/load_gameinfo.sh"
-source "$step/clean_game_prefix.sh"
+source "$STEP/load_gameinfo.sh"
+source "$STEP/clean_game_prefix.sh"
 
-install_dir=$(source "$step/select_install_dir.sh")
-log_info "selected install directory '$install_dir'"
+INSTALL_DIR=$(source "$STEP/select_install_dir.sh")
+log_info "selected install directory '$INSTALL_DIR'"
 
-expect_exit=0
+EXPECT_EXIT=0
 
-source "$step/download_external_resources.sh"
-source "$step/install_external_resources.sh"
-source "$step/install_nxm_handler.sh"
-source "$step/configure_steam_wineprefix.sh"
-source "$step/install_steam_redirector.sh"
-source "$step/register_installation.sh"
+source "$STEP/download_external_resources.sh"
+source "$STEP/install_external_resources.sh"
+source "$STEP/install_nxm_handler.sh"
+source "$STEP/configure_steam_wineprefix.sh"
+source "$STEP/install_steam_redirector.sh"
+source "$STEP/register_installation.sh"
 
-source "$step/apply_workarounds.sh"
+source "$STEP/apply_workarounds.sh"
 
 log_info "installation completed successfully"
-expect_exit=1
-"$dialog" infobox "Installation successful!\n\Launch the game on Steam to use Mod Organizer 2"
-
+EXPECT_EXIT=1
+"$DIALOG" infobox "Installation successful!\n\Launch the game on Steam to use Mod Organizer 2"
