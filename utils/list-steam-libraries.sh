84b5e920d1b292ee3b45edb8e99d518318bcc0c1
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-09 21:37:49 +0200

    Update list-steam-libraries.sh

--- utils/list-steam-libraries.sh
+++ utils/list-steam-libraries.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
 function log_info() {
-	echo "INFO:" "$@" >&2
+    echo "INFO:" "$@" >&2
 }
 
-steam_install_candidates=( \
-	"$(readlink -f "$HOME/.steam/root")" \
-	"$HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam" \
+steam_install_candidates=(
+    "$(readlink -f "$HOME/.steam/root")"
+    "$HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam"
 )
 
 for steam_install in "${steam_install_candidates[@]}"; do
-	if [ -d "$steam_install" ]; then
-		log_info "found Steam in '$steam_install'"
+    if [ -d "$steam_install" ]; then
+        log_info "found Steam in '$steam_install'"
 
-		if [ -d "$steam_install/steamapps" ]; then
-			main_library="$steam_install"
-		else
-			main_library="$steam_install/steam"
-		fi
+        if [ -d "$steam_install/steamapps" ]; then
+            main_library="$steam_install"
+        else
+            main_library="$steam_install/steam"
+        fi
 
-		grep -oE '/[^"]+' "$main_library/steamapps/libraryfolders.vdf"
-	else
-		log_info "steam not found in '$steam_install'"
-	fi
+        if [ -f "$main_library/steamapps/libraryfolders.vdf" ]; then
+            grep -oE '/[^"]+' "$main_library/steamapps/libraryfolders.vdf"
+        else
+            log_info "libraryfolders.vdf not found in '$main_library/steamapps'"
+        fi
+    else
+        log_info "steam not found in '$steam_install'"
+    fi
 done
-
