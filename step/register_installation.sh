d363d0d505f73f26a7b66059bb9cf71a8e7fe768
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-20 21:46:57 +0200

    Update register_installation.sh

--- step/register_installation.sh
+++ step/register_installation.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-mkdir -p "$HOME/.config/modorganizer2/instances"
-rm -f "$HOME/.config/modorganizer2/instances/$game_nexusid"
-ln -s "$install_dir" "$HOME/.config/modorganizer2/instances/$game_nexusid"
+# Creates the directory structure for Mod Organizer 2 instances if it doesn't already exist.
+# This directory is used by Mod Organizer 2 to manage different game instances.
+# Parameters:
+#   - HOME: The user's home directory, used to determine the path to the Mod Organizer 2 configuration directory.
 
+mkdir -p "$HOME/.config/modorganizer2/instances"
+
+# Removes any existing symbolic link for the game instance in the Mod Organizer 2 instances directory.
+# This ensures that the link points to the correct installation directory.
+# Parameters:
+#   - HOME: The user's home directory, used to determine the path to the Mod Organizer 2 configuration directory.
+#   - game_nexusid: The Nexus Mods ID of the game, used to name the symbolic link.
+
+rm -f "$HOME/.config/modorganizer2/instances/$game_nexusid"
+
+# Creates a symbolic link in the Mod Organizer 2 instances directory pointing to the game's installation directory.
+# This link allows Mod Organizer 2 to manage the game's mods and configurations.
+# Parameters:
+#   - install_dir: The directory where the game is installed.
+#   - HOME: The user's home directory, used to determine the path to the Mod Organizer 2 configuration directory.
+#   - game_nexusid: The Nexus Mods ID of the game, used to name the symbolic link.
+
+ln -s "$install_dir" "$HOME/.config/modorganizer2/instances/$game_nexusid"
