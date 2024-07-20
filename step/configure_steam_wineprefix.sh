f6b2317c8547b684a5880f8795d46277eada6e45
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-20 21:44:46 +0200

    Update configure_steam_wineprefix.sh

--- step/configure_steam_wineprefix.sh
+++ step/configure_steam_wineprefix.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+# Checks if there are any Protontricks specified for the game.
+# If so, it applies them to the game's Proton prefix.
+# Parameters:
+#   - game_protontricks: An array of Protontricks to be applied.
+#   - game_appid: The app ID of the game, used to identify the game's Proton prefix.
+#   - utils: The directory containing utility scripts, such as protontricks.sh.
+#   - dialog: The path to a dialog utility used for displaying messages to the user.
+
 if [ -n "${game_protontricks[*]}" ]; then
 	log_info "applying protontricks ${game_protontricks[@]}"
+
+	# Applies the specified Protontricks to the game's Proton prefix.
+	# Displays a loading dialog while the Protontricks are being applied.
+	# If the Protontricks application fails, an error dialog is shown, and the script exits with a non-zero status.
 
 	"$utils/protontricks.sh" apply "$game_appid" "${game_protontricks[@]}" \
 		| "$dialog" loading "Configuring game prefix\nThis may take a while"
@@ -12,4 +24,3 @@ if [ -n "${game_protontricks[*]}" ]; then
 		exit 1
 	fi
 fi
-
